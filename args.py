760073b3a944ff06613826425f9c91eac0a82f35
leo27945875 <ezioatiar@gmail.com>
2020-06-16 23:52:33 +0800

    樹梅派位置調整到交大工二館

--- args.py
+++ args.py
@@ -23,7 +23,7 @@ def Init():
     coordinateData = root+"coordinate.json"
 
     # Flood model setting:
-    start = (249761.6, 2742381.8)
+    start = (249760.8, 2742380.8)
     startOnGrid = False
 
     # Time zone:
